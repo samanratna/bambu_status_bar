e52e1af965713887c24a0370b1c16fdbccfa9a78
samanratna <samanratna2018@gmail.com>
2026-05-17 13:57:41 -0400

    add python script to check APIs before trying it on ESP's code: connection and MQTT working

---
+++ python_test_scripts/credentials.py
@@ -0,0 +1,9 @@
+# Abhay dai's printer credentials
+IP = '10.0.0.52'
+SERIAL = '03919D540809390'
+ACCESS_CODE = '27665761'
+
+# # IDC's printer credentials
+# IP = '10.0.0.52'
+# SERIAL = '03919D540809390'
+# ACCESS_CODE = '27665761'
