d681bc927544ff0ce1092daa92b1500f27602dd2
samanratna <samanratna2018@gmail.com>
2026-06-03 17:25:06 -0400

    test script in python (ready to use)

--- python_test_scripts/api_test_python_script.py
+++ python_test_scripts/api_test_python_script.py
@@ -11,42 +11,43 @@ if __name__ == '__main__':
     print(f'Access Code: {ACCESS_CODE}')
 
     # Create a new instance of the API
-    
     printer = bl.Printer(IP, ACCESS_CODE, SERIAL)
 
     # Start the MQTT client to receive real-time updates from the printer
     printer.mqtt_start()
 
-    time.sleep(2)
+    for i in range(50):
+        time.sleep(2)
 
-    # Get the printer status
-    status = printer.get_state()
-    print(f'Printer status: {status}')
+        # Get the printer status
+        status = printer.get_state()
+        print(f'Printer status: {status}')
 
-    percentage = printer.get_percentage()
-    layer_num = printer.current_layer_num()
-    total_layer_num = printer.total_layer_num()
-    bed_temperature = printer.get_bed_temperature()
-    nozzle_temperature = printer.get_nozzle_temperature()
-    
-    remaining_time = printer.get_time()
-    if remaining_time is not None:
-        finish_time = datetime.datetime.now() + datetime.timedelta(
-            minutes=int(remaining_time))
-        finish_time_format = finish_time.strftime("%Y-%m-%d %H:%M:%S")
-    else:
-        finish_time_format = "NA"
+        percentage = printer.get_percentage()
+        layer_num = printer.current_layer_num()
+        total_layer_num = printer.total_layer_num()
+        bed_temperature = printer.get_bed_temperature()
+        nozzle_temperature = printer.get_nozzle_temperature()
+        
+        remaining_time = printer.get_time()
+        if remaining_time is not None:
+            finish_time = datetime.datetime.now() + datetime.timedelta(
+                minutes=int(remaining_time))
+            finish_time_format = finish_time.strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            finish_time_format = "NA"
 
-    print(
-        f'''Printer status: {status}
-        Layers: {layer_num}/{total_layer_num}
-        percentage: {percentage}%
-        Bed temp: {bed_temperature} ºC
-        Nozzle temp: {nozzle_temperature} ºC
-        Remaining time: {remaining_time}m
-        Finish time: {finish_time_format}
-        '''
-    )
+        print(
+            f'''Printer status: {status}
+            Layers: {layer_num}/{total_layer_num}
+            percentage: {percentage}%
+            Bed temp: {bed_temperature} ºC
+            Nozzle temp: {nozzle_temperature} ºC
+            Remaining time: {remaining_time}m
+            Finish time: {finish_time_format}
+            '''
+        )
 
     # # Disconnect from the Bambulabs 3D printer
     printer.disconnect()
+
--- python_test_scripts/credentials.py
+++ python_test_scripts/credentials.py
@@ -1,9 +1,26 @@
-# Abhay dai's printer credentials
-IP = '10.0.0.52'
-SERIAL = '03919D540809390'
-ACCESS_CODE = '27665761'
+# # Abhay dai's printer credentials
+# IP = '10.0.0.52'
+# SERIAL = '03919D540809390'
+# ACCESS_CODE = '27665761'
 
 # # IDC's printer credentials
 # IP = '10.0.0.52'
 # SERIAL = '03919D540809390'
 # ACCESS_CODE = '27665761'
+
+# IDC's printer credentials PS-1
+IP = '35.39.176.95'
+SERIAL = '01P09C4C0701535'
+ACCESS_CODE = '150208334'
+
+# IDC's printer credentials PS-2
+# IP = '35.39.176.1'
+# # SERIAL = '00600A4C1101658'
+# SERIAL = '01P00C510700935'
+# ACCESS_CODE = '2c4416e6'
+
+# # IDC's printer credentials PS-3
+# IP = '35.39.176.3'
+# SERIAL = '01P00C510700965'
+# ACCESS_CODE = '16831792'
+
